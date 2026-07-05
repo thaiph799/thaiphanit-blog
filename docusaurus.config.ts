b45f969187d4fe4a35c0992bb67a2de4e7c84072
Thai Phan <thai.phan5@rmit.edu.vn>
2026-07-05 17:07:23 +0700

    Remove edit page link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+presets
   presets: [
     [
       'classic',
@@ -34,7 +34,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            '',
         },
         blog: {
           showReadingTime: true,
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            '',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
